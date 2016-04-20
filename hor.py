94f1e51a2cece20fcb25bb8b55b7610252c4746f
Christopher Sipes <crs@Christophers-MacBook-Pro.local>
2016-04-20 16:57:14 -0700

    Got the spring moving

--- hor.py
+++ hor.py
@@ -13,11 +13,13 @@ deltat = 0.1
 g = 9.81
 horSceneCenter = (0, 0, 0)
 verSceneCenter = (0, -0.9, 0)
-horSpringLength = (0.05, 0, 0)
+horSpringLength = vector(1, 0, 0)
+relaxedLength = vector(0.5, 0, 0)
+stretchOffset = vector(0.1001, 0, 0)
 
 ### configurables
 k = 0.1
-stretch = 5
+stretch = 0.05
 boxMass = 1
 horDispConst = vector(0.1001, 0, 0)
 
@@ -25,28 +27,23 @@ horDispConst = vector(0.1001, 0, 0)
 global isRunning
 isRunning = False
 
-# visual elements
-spring = None
-wall = None
-ground = None
-sBox = None
+def calcAxis(relaxedLength, boxPos, stretchOffset):
+    return relaxedLength + boxPos + stretchOffset
 
 # class to manage the configurables
 class SpringData(object):
     associatedLabel = None
+    spring = None
     orientation = ''
     k = 0.1
     stretch = 0
-    # stretchOffset = ()
 
-    def __init__(self, associatedLabel, spring, sBox, orientation, k, stretch):
+    def __init__(self, associatedLabel, spring, orientation, k, stretch):
         self.associatedLabel = associatedLabel
         self.spring = spring
-        self.sBox = sBox
         self.orientation = 'horizontal'
         self.k = k
         self.springStretch = stretch
-        self.stretchOffset = vector(0.1001, 0, 0)
 
     def generateText(self):
         line1 = 'Orientation : ' + str(self.orientation) + '\n'
@@ -67,7 +64,7 @@ class SpringData(object):
         self.orientation = orientation
 
     def getK(self):
-        return self.orientation
+        return self.k
 
     def setK(self, k):
         self.k = k
@@ -78,6 +75,12 @@ class SpringData(object):
     def setSpringStretch(self, stretch):
         self.springStretch = stretch
 
+    def getSpring(self):
+        return self.spring
+
+    def setSpring(self, spring):
+        self.spring = spring
+
 def setK(sliderK, springData):
     springData.setK(sliderK.value)
     springData.updateAssociatedLabelText()
@@ -86,21 +89,36 @@ def setStretch(sliderStretch, springData):
     springData.setSpringStretch(sliderStretch.value)
     springData.updateAssociatedLabelText
 
-def runDemo():
+def runDemo(springData, spring, sBox, deltat, boxMass, relaxedLength):
     global isRunning
     if (isRunning == False):
         isRunning = True
-        print "Running now"
-    else:
-        print "already running"
 
+        count = 0
+        pBox = vector(0, 0, 0)
+        while (count < 1000):
+            rate(100)
+            Fnet = -1 * springData.getK() * (sBox.pos - horSpringLength)
+            print Fnet
+            pBox = pBox + Fnet * deltat
+            sBox.pos = sBox.pos + (pBox / boxMass) * deltat
+            spring.axis = sBox.pos + relaxedLength + stretchOffset
+            count += 1
+
+    isRunning = False
+
+# visual elements
+sBox = box(pos = horSpringLength + vector(stretch, 0, 0), size = (0.3,0.3,0.3), color = color.yellow)
+spring = helix(pos = vector(-0.75,0,0), axis = calcAxis(relaxedLength, sBox.pos, stretchOffset), radius = 0.1, coils = 8, thickness = 0.01, color = color.red)
+ground = box(size=(1.5,0.02,0.5), pos=(-0.05,-0.16,0))
+wall = box(size=(0.04,0.5,0.3),pos=(-0.77,0.1,0),color=color.white)
 
 attributesLabel = label(pos = scene.center, color = color.white, height = 10, border = 6, display = scene)
-springData = SpringData(attributesLabel, spring, sBox, 'horizontal', k, stretch)
+springData = SpringData(attributesLabel, spring, 'horizontal', k, stretch)
 springData.updateAssociatedLabelText()
 
 c = controls(title="Configure Spring Attributes", width=300, height=400)
 sliderK = slider(pos = (-50, 0), width=7, length=120, axis=(1,0), min=0.1, max=10, text="Spring constant", action=lambda: setK(sliderK, springData), color = color.red, value = k)
 sliderStretch = slider(pos = (-50, 10), width=7, length=120, axis=(1,0), min=0.1, max=10, text="Spring constant", action=lambda: setStretch(sliderStretch, springData), color = color.blue, value = stretch)
 buttonRun = button( pos=(0,60), width=120, height=40, border=0,
-              text='Run Demo', action=lambda: runDemo() )
+              text='Run Demo', action=lambda: runDemo(springData, spring, sBox, deltat, boxMass, relaxedLength) )
